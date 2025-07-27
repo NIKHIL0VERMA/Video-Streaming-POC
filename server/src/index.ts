0c2b7b3c629a7af1c40df1fc563c2bc9873257c1
NIKHIL0VERMA <nikhil2003verma@gmail.com>
2025-07-27 23:55:56 +0530

    upload and conversion is working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import Express from "express";
 import cors from "cors";
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { exec } from "child_process"
 
 const app = Express();
 const port = 8000;
@@ -16,7 +19,7 @@ app.use(Express.urlencoded({ extended: true }));
 // File upload setup
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, "uploads/raw/");
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
@@ -25,6 +28,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// Serve static files
+app.use("/uploads", Express.static(path.join(__dirname, "../uploads")));
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the video streaming server!");
@@ -32,6 +38,33 @@ app.get("/", (req, res) => {
 
 app.head("/health", (req, res) => {
   res.status(200).send("OK");
+});
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  const videoPath = req.file.path;
+  const outputPath = `uploads/${req.file.filename.split('.')[0]}`;
+  const main = `${outputPath}/main.m3u8`;
+  if(!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+  }
+
+  // in production ye to asyncronous hi krna hoga event loop block nhi krna pure server ka ;)
+  // Koi fffmpeg acha comman btao bhai, production me to aac hoga or kya pta krna hoga
+  exec(`ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/part%02d.ts" -start_number 0 ${main}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error processing video: ${error.message}`);
+      return res.status(500).json({ error: "Error processing video" });
+    }
+    if (stderr) {
+      console.error(`FFmpeg stderr: ${stderr}`);
+    }
+    console.log(`FFmpeg stdout: ${stdout}`);
+    const url = `http://localhost:${port}/${outputPath}/main.m3u8`;
+    res.status(200).json({ message: "Video processed successfully", url: url });
+  }); 
 });
 
 app.listen(port, () => {
